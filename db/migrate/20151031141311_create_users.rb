61b929dcbeef3476748a12c51b51631bbfd693bb
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2015-11-19 18:27:34 +0900

    :bug: 初回にAPIから読み込みを行う場合のバグを修正
    since_idは0とならないため、初期値を1にする

--- db/migrate/20151031141311_create_users.rb
+++ db/migrate/20151031141311_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :screen_name, :index => true, :unique => true
-      t.string :since_id, :default => 0
+      t.string :since_id, :default => 1
       t.string :access_token
       t.string :access_token_secret
     end
